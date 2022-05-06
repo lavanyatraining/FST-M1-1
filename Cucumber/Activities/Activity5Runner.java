96fd6c64416bdf7b0e31feaa603614d50b23f427
lavanyatraining <104412090+lavanyatraining@users.noreply.github.com>
2022-05-06 17:11:53 +0530

    Cucumber Activity Runner files

---
+++ Cucumber/Activities/Activity5Runner.java
@@ -0,0 +1,16 @@
+package cucumberTests;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+features = "Features",
+glue = {"stepDefinitions"},
+tags = "@activity4"
+)
+public class Activity5Runner {
+
+}
